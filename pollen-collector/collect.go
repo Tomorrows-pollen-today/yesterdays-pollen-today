fa8b1eaa1cc55cc2ece76f71171283546c062bf9
Lasse Valentini Jensen <lasse.valentini@gmail.com>
2018-09-13 20:53:30 +0200

    Added functionality to fetch a full history of pollen values and predictions from a separate Azure api

--- pollen-collector/collect.go
+++ pollen-collector/collect.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -22,6 +23,8 @@ import (
 var config *CollectorConfig
 
 func main() {
+	fullHistory := flag.Bool("full-history", false, "Fetch historical data")
+	flag.Parse()
 	// Change directory to the same as executable
 	executable, _ := os.Executable()
 	exPath := filepath.Dir(executable)
@@ -35,6 +38,21 @@ func main() {
 	defer pollenRepo.Close()
 
 	pollenRepo.InitDb()
+
+	if *fullHistory {
+		log.Println("Collecting full history")
+		historicalPollen, err := getHistoricalPollen()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		var count = len(historicalPollen)
+		log.Printf("Found %v historical pollendates", count)
+		for index := 0; index < count; index++ {
+			pollenRepo.UpsertPollenDate(historicalPollen[index])
+		}
+		return
+	}
 
 	var waitGroup sync.WaitGroup
 
@@ -114,23 +132,24 @@ func getTomorrowsPollen() (float32, error) {
 	if err != nil {
 		log.Fatal(err, tomorrowsPollenResponse)
 		return 0, err
-	} else {
-		defer tomorrowsPollenResponse.Body.Close()
-		data, _ := ioutil.ReadAll(tomorrowsPollenResponse.Body)
-
-		var tomorrowsPollen azurePollenResponse
-		err = json.Unmarshal(data, &tomorrowsPollen)
-		if err != nil {
-			log.Println(err, tomorrowsPollenResponse, tomorrowsPollenResponse.Body)
-			return 0, err
-		}
-		tomorrowsPollenValue, err := strconv.ParseFloat(tomorrowsPollen.Results.PredictedPollenCount.Value.Values[0][0], 32)
-		if err != nil {
-			log.Println(err, tomorrowsPollenResponse)
-			return 0, err
-		}
-		return float32(tomorrowsPollenValue), nil
 	}
+
+	defer tomorrowsPollenResponse.Body.Close()
+	data, _ := ioutil.ReadAll(tomorrowsPollenResponse.Body)
+
+	var tomorrowsPollen azurePollenResponse
+	err = json.Unmarshal(data, &tomorrowsPollen)
+	if err != nil {
+		log.Println(err, tomorrowsPollenResponse, tomorrowsPollenResponse.Body)
+		return 0, err
+	}
+	tomorrowsPollenValue, err := strconv.ParseFloat(tomorrowsPollen.Results.PredictedPollenCount.Value.Values[0][0], 32)
+	if err != nil {
+		log.Println(err, tomorrowsPollenResponse)
+		return 0, err
+	}
+	return float32(tomorrowsPollenValue), nil
+
 }
 
 func getTodaysPollen(city string, pollenType string) (int32, error) {
@@ -163,4 +182,78 @@ func getTodaysPollen(city string, pollenType string) (int32, error) {
 	}
 
 	return 0, fmt.Errorf("Could not find pollen value for %v in %v", pollenType, city)
+}
+
+type azureHistoricalPollenResponse struct {
+	Results struct {
+		HistoricalPollenCount struct {
+			Type  string `json:"type"`
+			Value struct {
+				ColumnNames []string   `json:"ColumnNames"`
+				ColumnTypes []string   `json:"ColumnTypes"`
+				Values      [][]string `json:"Values"`
+			} `json:"value"`
+		} `json:"historical_pollen_count"`
+	} `json:"Results"`
+}
+
+func getHistoricalPollen() ([]*dataaccess.PollenDate, error) {
+	client := &http.Client{}
+	postBody, err := json.Marshal(map[string]interface{}{"GlobalParameters": map[string]string{}})
+	if err != nil {
+		panic(fmt.Errorf("json.Marshal: %v", err))
+	}
+
+	request, err := http.NewRequest("POST", config.HistoricalAPIEndpoint, bytes.NewBuffer(postBody))
+	if err != nil {
+		panic(fmt.Errorf("NewRequest: %v", err))
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %v", config.HistoricalAPIKey))
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("Content-Type", "application/json")
+
+	historicalPollenResponse, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err, historicalPollenResponse)
+		return nil, err
+	}
+	defer historicalPollenResponse.Body.Close()
+	data, _ := ioutil.ReadAll(historicalPollenResponse.Body)
+
+	var historicalPollen azureHistoricalPollenResponse
+	err = json.Unmarshal(data, &historicalPollen)
+	if err != nil {
+		log.Println(err, historicalPollenResponse, string(data))
+		return nil, err
+	}
+	var result []*dataaccess.PollenDate
+	var count = len(historicalPollen.Results.HistoricalPollenCount.Value.Values)
+	for index := 0; index < count; index++ {
+		var currentResult = historicalPollen.Results.HistoricalPollenCount.Value.Values[index]
+		date, err := time.Parse("1/2/2006 15:04:05 AM", currentResult[0])
+		if err != nil {
+			log.Println(err, currentResult)
+			continue
+		}
+		historicalPollenValue, err := strconv.ParseInt(currentResult[1], 10, 32)
+		if err != nil {
+			log.Println(err, currentResult)
+			continue
+		}
+		historicalPredictedPollenValue, err := strconv.ParseFloat(currentResult[2], 32)
+		if err != nil {
+			log.Println(err, currentResult)
+			continue
+		}
+		var pollenCount = int32(historicalPollenValue)
+		var predictedPollenCount = float32(historicalPredictedPollenValue)
+		var pollenDate = &dataaccess.PollenDate{
+			Date:                 dataaccess.TimestampToDate(date),
+			PollenCount:          &pollenCount,
+			PredictedPollenCount: &predictedPollenCount,
+		}
+		result = append(result, pollenDate)
+	}
+	return result, nil
+
 }
